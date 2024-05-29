9b0253dc7516f54c7678842e7ce92e4da0780306
maguoxin1234 <13023676283@163.com>
2024-05-29 16:30:50 +0800

    version 1.1.4: 推送api支持自定义header

--- java/src/main/java/com/aliyun/ha3engine/vector/Client.java
+++ java/src/main/java/com/aliyun/ha3engine/vector/Client.java
@@ -231,8 +231,11 @@ public class Client {
      * 支持新增、更新、删除 等操作，以及对应批量操作
      */
     public PushDocumentsResponse pushDocuments(String dataSourceName, String keyField, PushDocumentsRequest request) throws Exception {
-        request.headers = TeaConverter.buildMap(
-            new TeaPair("X-Opensearch-Swift-PK-Field", keyField)
+        request.headers = TeaConverter.merge(String.class,
+            TeaConverter.buildMap(
+                new TeaPair("X-Opensearch-Swift-PK-Field", keyField)
+            ),
+            request.headers
         );
         return TeaModel.toModel(this._request("POST", "/update/" + dataSourceName + "/actions/bulk", null, request.headers, request.body, this.buildRuntimeOptions()), new PushDocumentsResponse());
     }
