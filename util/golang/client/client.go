112326a0c32f1f89f20c1ba9aff6ed6867b0d426
党参 <yongxing.dyx@alibaba-inc.com>
2024-12-25 16:46:29 +0800

    add util

---
+++ util/golang/client/client.go
@@ -0,0 +1,19 @@
+// This file is auto-generated, don't edit it. Thanks.
+/**
+ * Compress data by specified compress type, use isCompressorAvailable to check if the compress type is supported.
+ * @param src the data to be compressed
+ * @param compressType the compress type
+ * @return the compressed data
+ * @throws error if the compress type is not supported or the compress failed
+ */
+package client
+
+import (
+  "github.com/alibabacloud-go/tea/tea"
+)
+
+
+func DeflateCompress (src []byte) (_result []byte, _err error) {
+  panic("No Support!")
+}
+
---
+++ util/python/alibabacloud_ha3_util/client.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+class Client:
+    """
+    Compress data by specified compress type, use isCompressorAvailable to check if the compress type is supported.
+    @param src the data to be compressed
+    @param compressType the compress type
+    @return the compressed data
+    @throws error if the compress type is not supported or the compress failed
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def deflate_compress(
+        src: bytes,
+    ) -> bytes:
+        raise Exception('Un-implemented')
+
+    @staticmethod
+    async def deflate_compress_async(
+        src: bytes,
+    ) -> bytes:
+        raise Exception('Un-implemented')
