fa26639bec7502e1c27baef7e2055a4fc8915b7e
lyk331 <liuyankai.lyk@alibaba-inc.com>
2026-04-02 14:45:37 +0800

    version1.1.18: 融合向量

--- java/src/main/java/com/aliyun/ha3engine/vector/models/QueryRequest.java
+++ java/src/main/java/com/aliyun/ha3engine/vector/models/QueryRequest.java
@@ -55,6 +55,12 @@ public class QueryRequest extends TeaModel {
     public String content;
 
     /**
+     * <p>需要预测的数据（向量融合场景)</p>
+     */
+    @NameInMap("contents")
+    public java.util.List<String> contents;
+    
+    /**
      * <p>使用的模型</p>
      */
     @NameInMap("modal")
@@ -201,6 +207,14 @@ public class QueryRequest extends TeaModel {
         return this.content;
     }
 
+    public QueryRequest setContents(java.util.List<String> contents) {
+        this.contents = contents;
+        return this;
+    }
+    public java.util.List<String> getContents() {
+        return this.contents;
+    }
+    
     public QueryRequest setModal(String modal) {
         this.modal = modal;
         return this;
